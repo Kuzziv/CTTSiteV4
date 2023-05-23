b3c04fc965e623112c5134f50a6f226f008d2233
c-canty <christian_canty@hotmail.co.uk>
2023-05-23 13:25:38 +0200

    Mock Consultations

--- Program/MockData/MockDataConsultation.cs
+++ Program/MockData/MockDataConsultation.cs
@@ -7,11 +7,11 @@ namespace CTTSite.MockData
 
         public static List<Consultation> consultationsList = new List<Consultation>()
         {
-            //new Consultation(1, "10-05-2023", new TimeSpan(10, 0, 0), new TimeSpan(11, 0, 0), 1, " ", " ", " "),
-            //new Consultation(2, "10-05-2023", new TimeSpan(11, 0, 0), new TimeSpan(12, 0, 0), 1, " ", " ", " "),
-            //new Consultation(3, "10-05-2023", new TimeSpan(12, 0, 0), new TimeSpan(13, 0, 0), 1, " ", " ", " "),
-            //new Consultation(4, "10-05-2023", new TimeSpan(13, 0, 0), new TimeSpan(14, 0, 0), 1, " ", " ", " "),
-            //new Consultation(5, "10-05-2023", new TimeSpan(14, 0, 0), new TimeSpan(15, 0, 0), 1, " ", " ", " ")
+            new Consultation(1, DateTime.Now.Date, new TimeSpan(10, 0, 0), new TimeSpan(11, 0, 0), 1, " ", " ", " ", false),
+            new Consultation(2, DateTime.Now.Date, new TimeSpan(12, 0, 0), new TimeSpan(13, 0, 0), 1, " ", " ", " ", false),
+            new Consultation(3, DateTime.Now.Date, new TimeSpan(14, 0, 0), new TimeSpan(15, 0, 0), 1, " ", " ", " ", false),
+            new Consultation(4, DateTime.Now.Date, new TimeSpan(16, 0, 0), new TimeSpan(17, 0, 0), 1, " ", " ", " ", false),
+            new Consultation(5, DateTime.Now.Date, new TimeSpan(18, 0, 0), new TimeSpan(19, 0, 0), 1, " ", " ", " ", false)
         };
 
         public static List<Consultation> GetAllConsultations()
